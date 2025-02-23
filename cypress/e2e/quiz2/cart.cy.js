dd369451868d6c8b1605bb73a76c741dfaaa0e30
mifqiii <mifqiii@gmail.com>
2025-02-23 17:57:08 +0700

    comiit pertama kali untuk cart

--- cypress/e2e/quiz2/cart.cy.js
+++ cypress/e2e/quiz2/cart.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Shopping Cart > Add, edit or Remove Products in the Cart', () => {
+  beforeEach(() => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS9jdXN0b21lci9hY2NvdW50L2xvZ291dFN1Y2Nlc3Mv/')
+    cy.get('#email').type('kelompok2@mail.com');
+    cy.get('#pass').type('Kelompok2');
+    cy.get('#send2').click(); 
   })
+
+  it('Positive Case: Edit Account Information Successfully', () => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/edit/')
+    cy.get('#firstname').clear().type('Kelompok')
+    cy.get('#lastname').clear().type('Dua Sanber')
+    cy.get('button[title="Save"]').click()
+    cy.contains('You saved the account information.').should('be.visible')
+  })
+
 })
