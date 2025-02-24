96966cb22b2b32dea8e3835eda3d1aef9a6a25d5
annizhafizhah <annizhamulh@gmail.com>
2025-02-24 10:51:17 +0700

    update my account

--- cypress/e2e/quiz2/myaccount.cy.js
+++ cypress/e2e/quiz2/myaccount.cy.js
@@ -3,7 +3,7 @@ describe('My Account > Edit Account Information & Edit Address', () => {
     cy.visit('https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS9jdXN0b21lci9hY2NvdW50L2xvZ291dFN1Y2Nlc3Mv/')
     cy.get('#email').type('kelompok2@mail.com');
     cy.get('#pass').type('Kelompok2');
-    cy.get('#send2').click(); 
+    cy.get('.login[name="send"]').click(); 
   })
 
   it('Positive Case: Edit Account Information Successfully', () => {
@@ -13,14 +13,15 @@ describe('My Account > Edit Account Information & Edit Address', () => {
     cy.get('button[title="Save"]').click()
     cy.contains('You saved the account information.').should('be.visible')
   })
-  it('Negative Case: Edit Account Information without First Name', () => {
+
+  it('Negative Case: change Account Information without First Name', () => {
     cy.visit('https://magento.softwaretestingboard.com/customer/account/edit/')
     cy.get('#firstname').clear()
     cy.get('button[title="Save"]').click()
     cy.contains('This is a required field.').should('be.visible')
   })
 
-  it('Negative Case: Edit Account Information without Last Name', () => {
+  it('Negative Case: change Account Information without Last Name', () => {
     cy.visit('https://magento.softwaretestingboard.com/customer/account/edit/')
     cy.get('#lastname').clear()
     cy.get('button[title="Save"]').click()
